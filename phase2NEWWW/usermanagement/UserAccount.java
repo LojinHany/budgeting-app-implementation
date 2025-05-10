62bfce75e1a861869af6de1916909a111248c1b2
SomayaTamer <160321575+SomayaTamer@users.noreply.github.com>
2025-05-10 07:22:56 -1200

    Update UserAccount.java

--- phase2NEWWW/usermanagement/UserAccount.java
+++ phase2NEWWW/usermanagement/UserAccount.java
@@ -2,6 +2,11 @@ package usermanagement;
 
 import java.util.Scanner;
 
+import persistenceLayer.UltraSimpleUserStorage;
+
+import java.util.List;
+import java.util.Map;
+
 public class UserAccount { // 1 to 1 multplicity with user and composition with user
     private User user; 
     private Scanner scanner = new Scanner(System.in);
@@ -41,39 +46,59 @@ public class UserAccount { // 1 to 1 multplicity with user and composition with 
         return false;
     }    
 
-
     public boolean changePassword() {
-        System.out.print("\nEnter current password: ");
-        String currentPassword = scanner.nextLine();
+        try {
+            System.out.print("\nEnter your email: ");
+            String email = scanner.nextLine().trim();
 
-        if (!user.getPassword().equals(currentPassword)) {
-            System.out.println("Incorrect current password. Password change failed.");
+            System.out.print("\nEnter current password: ");
+            String currentPassword = scanner.nextLine();
+
+            if (!user.getPassword().equals(currentPassword)) {
+                System.out.println("Incorrect current password. Password change failed.");
+                return false;
+            }
+
+            System.out.print("Enter new password: ");
+                String pass1 = scanner.nextLine();
+                while (!validPass(pass1)){
+                    System.out.print("Enter new password: ");
+                    pass1 = scanner.nextLine();
+                }
+                
+                System.out.print("Confirm your password: ");
+                String pass2 = scanner.nextLine();
+
+                while (!pass1.equals(pass2)) {
+                    System.out.println("Passwords do not match.\nPlease Recreate your Password");
+                    System.out.print("Enter new password: ");
+                    pass1 = scanner.nextLine();
+                    while (!validPass(pass1)){
+                        System.out.print("Enter new password: ");
+                        pass1 = scanner.nextLine();
+                    }
+                    System.out.print("Confirm your password: ");
+                    pass2 = scanner.nextLine();
+                }
+
+            // Load all users
+            List<User> allUsers = UltraSimpleUserStorage.loadUsers();
+
+            for (User u : allUsers) {
+                if (u.getEmailAddress().equals(email)) { 
+                    u.setPassword(pass1);
+                    break;
+                }
+            }
+            UltraSimpleUserStorage.saveUsers(allUsers);
+
+            System.out.println("Password successfully changed!");
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error during password change: " + e.getMessage());
             return false;
         }
-
-        System.out.print("Enter new password: ");
-        String pass1 = scanner.nextLine();
-        while (!validPass(pass1)){
-            System.out.print("Enter new password: ");
-            pass1 = scanner.nextLine();
-        }
-            
-        System.out.print("Confirm new password: ");
-        String pass2 = scanner.nextLine();
-
-        while (!pass1.equals(pass2)) {
-            System.out.println("Passwords do not match. Signup failed.\nPlease Recreate your Password");
-            System.out.print("Create your password: ");
-            pass1 = scanner.nextLine();
-            System.out.print("Confirm your password: ");
-            pass2 = scanner.nextLine();
-        }
-
-        user.setPassword(pass1);
-        System.out.println("Password successfully changed!");
-        return true;
     }
-
 
     public boolean updateEmail() {
         System.out.print("\nEnter current email: ");
@@ -98,12 +123,60 @@ public class UserAccount { // 1 to 1 multplicity with user and composition with 
         return true;
     }
 
-
     public boolean setSecurityOptions() {
-        return true;
+        try {
+            // Load existing PINs
+            Map<String, String> userPins = UltraSimpleUserStorage.loadUserPins();
+
+            System.out.print("\nEnter your email: ");
+            String email = scanner.nextLine().trim();
+            // check if the user already has a pin
+            if (userPins.containsKey(email)) {
+                System.out.println("A PIN already exists.");
+                return false;
+            }
+
+            System.out.print("\nSet Your 4-digit Security Pin: ");
+            String pin = scanner.nextLine().trim();
+
+            // check if pin is 4 digits
+            while (!pin.matches("\\d{4}")){
+                System.out.println("Invalid! Please enter a 4-digit pin");
+                System.out.print("\nSet Your 4-digit Security Pin: ");
+                pin = scanner.nextLine().trim();
+            }
+            
+            System.out.print("Confirm your pin: ");
+            String pin2 = scanner.nextLine();
+
+            while (!pin.equals(pin2)) {
+                System.out.println("Invalid! Pins do not match.\nPlease Recreate your Pin");
+                System.out.print("Set Your 4-digit Security Pin: ");
+                pin = scanner.nextLine();
+
+                while (!pin.matches("\\d{4}")){
+                    System.out.println("Invalid! Please enter a 4-digit pin");
+                    System.out.print("\nSet Your 4-digit Security Pin: ");
+                    pin = scanner.nextLine().trim();
+                }
+
+                System.out.print("Confirm your pin: ");
+                pin2 = scanner.nextLine();
+            }
+            // Add user pin
+            userPins.put(email, pin);
+            UltraSimpleUserStorage.saveUserPins(userPins);
+
+            System.out.println("Your PIN has been successfully set.");
+            return true;
+
+        }catch (Exception e) {
+            System.out.println("Error during creating pin: " + e.getMessage());
+            return false;
+        }
     }
 
-    public boolean updateSecurityOptions() {
-        return true;
+    public void helpAndSupport(){
+        System.out.println("\nFor Customer Support call: 01150446626 /  01064830839\n");
     }
 }
